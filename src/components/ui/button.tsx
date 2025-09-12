0ca0750cd15c3f97d185909d5d634047e7b6eea6
KARAM-06Z <67804623+KARAM-06Z@users.noreply.github.com>
2025-09-12 11:01:24 +0300

    fix: extra style lines & boolean prop error

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -1,12 +1,12 @@
 import { Button as _Button } from "@/core/defs/button"
 import type { IButton } from "@/core/interfaces/i-button"
 
-const Button = ({ children, ...props }: React.ButtonHTMLAttributes<HTMLButtonElement> & IButton) => {
+const Button = ({ children, transparent, ...props }: React.ButtonHTMLAttributes<HTMLButtonElement> & IButton) => {
   const buttonProps = new _Button({ ...props }) as IButton
   return (
     <button
       {...props}
-      className={`btn btn-${buttonProps.background} ${buttonProps.transparent ? "btn-transparent" : ""} text-${buttonProps.size}${
+      className={`btn btn-${buttonProps.background} ${transparent ? "btn-transparent" : ""} text-${buttonProps.size}${
         props?.className ? ` ${props.className}` : ""
       }`}
     >
