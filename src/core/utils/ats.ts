48a73dbc5c228df50e32e0b69b3498a0847f6a82
KARAM-06Z <67804623+KARAM-06Z@users.noreply.github.com>
2025-10-04 21:16:02 +0300

    fix: ats 0 idx empty space & collapse list item margin

--- src/core/utils/ats.ts
+++ src/core/utils/ats.ts
@@ -1,7 +1,7 @@
 const ats = (arr: string[]) =>
   arr.reduce((acc, c) => {
     if (!c?.length) return acc
-    return acc + " " + c
+    return (acc.length ? acc + " " : "") + c
   }, "")
 
 export { ats }
