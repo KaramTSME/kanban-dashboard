f126265ad539769de8a2c59f84c38b40232f0315
KaramTSME <112618821+KaramTSME@users.noreply.github.com>
2025-11-18 15:14:46 +0300

    feat: improve FormControl API

--- src/components/ui/form-control.tsx
+++ src/components/ui/form-control.tsx
@@ -1,21 +1,43 @@
+import React from "react"
 import { isEqual } from "@/core"
-import { createContext, useMemo, useState } from "react"
 import type { TNullable } from "@/core/types/global"
 import type { TFormControlProps } from "./meta/form-control"
 import type { TFormControlContext } from "./meta/form-control-context"
 
-const FormControlContext = createContext<TNullable<TFormControlContext>>(null)
+const FormControlContext = React.createContext<TNullable<TFormControlContext>>(null)
 
-function FormControl<T>({ instance: inst, defaultFormValue = {}, ...props }: TFormControlProps<T>) {
-  const [instance, setInstance] = useState<T>(new inst({ ...defaultFormValue }))
-  const [originalValue] = useState({ ...instance })
+function FormControl<T>({ instance: inst, defaultFormValue = {}, onUpdateFire, onSettleFire, ...props }: TFormControlProps<T>) {
+  const initialInstance = new inst({ ...defaultFormValue })
+  const isControlMountedRef = React.useRef(false)
+  const hasChanged = React.useRef(false)
+  const [instance, setInstance] = React.useState<T>(initialInstance)
+  const [originalInstance, setOriginalInstance] = React.useState(initialInstance)
 
-  const isDirty = useMemo(() => !isEqual(instance, originalValue), [instance, originalValue])
+  const isDirty = React.useMemo(() => !isEqual(instance, originalInstance), [instance, originalInstance])
+
+  const update = React.useCallback((prop: any, value: T[keyof T]) => setInstance({ ...instance, [prop]: value }), [instance])
+
+  const rollback = React.useCallback(() => setInstance({ ...originalInstance }), [originalInstance])
+
+  const commit = React.useCallback(() => setOriginalInstance({ ...instance }), [instance])
+
+  React.useEffect(() => {
+    if (!isControlMountedRef.current) {
+      isControlMountedRef.current = true
+      return
+    }
+    if (isDirty) {
+      hasChanged.current = true
+      onUpdateFire?.(instance)
+    } else if (hasChanged.current) onSettleFire?.(instance)
+  }, [instance, isDirty, onUpdateFire, onSettleFire])
 
   const context: TFormControlContext = {
-    instance: instance,
+    instance,
     isDirty,
-    setInstance,
+    update,
+    rollback,
+    commit,
   }
 
   return <FormControlContext.Provider value={context} {...props} />
--- src/components/ui/meta/form-control-context.ts
+++ src/components/ui/meta/form-control-context.ts
@@ -1,3 +1,9 @@
-type TFormControlContext<T = any> = { instance: T; isDirty: boolean; setInstance: React.Dispatch<any> }
+type TFormControlContext<T = any> = {
+  instance: T
+  isDirty: boolean
+  update: (prop: keyof T, value: T[keyof T]) => void
+  rollback: () => void
+  commit: () => void
+}
 
 export type { TFormControlContext }
